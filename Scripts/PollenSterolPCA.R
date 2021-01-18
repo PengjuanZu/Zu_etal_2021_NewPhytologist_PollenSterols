173d7a8278dfbf5af1b682d888d8c2efb0a5a729
Pengjuan Zu <49073005+PengjuanZu@users.noreply.github.com>
2021-01-18 19:27:49 +0100

    Update PollenSterolPCA.R

--- Scripts/PollenSterolPCA.R
+++ Scripts/PollenSterolPCA.R
@@ -1,16 +1,14 @@
-require(ggbiplot)
-
-require(factoextra)
-
-require(stats)
-
-require(pracma)
+## load library
+library(ggbiplot)
+library(factoextra)
+library(stats)
+library(pracma)
 
 
 ##DATA_UPLOAD#########################################################################################################
 # Uploads the .csv file containing information on the % (i.e. relative abundance) of sterols in each pollen sample
 
-sterolRelAb<-read.table("E:/PollenSterolPercentPCA.csv",sep=",", header=TRUE)
+sterolRelAb<-read.table("~/PollenSterolPercentPCA.csv",sep=",", header=TRUE)
 head(sterolRelAb)
 
 pollSterol.pca<-prcomp(sterolRelAb[,c(2:26)], center=TRUE, scale.=TRUE)
